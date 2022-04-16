8dbd652d25dddffc8df3886c326aac577ff3815f
srtalaie <srt4721@yahoo.com>
2022-04-15 22:04:02 -0700

    2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,34 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
+        <div>debug: {newName}</div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleAddPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
+      <ul>
+        {persons.map((person) => 
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
     </div>
   )
 }
