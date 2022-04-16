32f010b486ba09446536f2e583ea6eaacc1c3661
srtalaie <srt4721@yahoo.com>
2022-04-15 22:16:51 -0700

    2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,8 +12,12 @@ const App = () => {
 
   const handleAddPerson = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({name: newName}))
-    setNewName('')
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({name: newName}))
+      setNewName('')
+    }
   }
 
   return (
