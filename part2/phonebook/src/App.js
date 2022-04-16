7c32c091b2f681a967aeee727bae4af8d6428467
srtalaie <srt4721@yahoo.com>
2022-04-15 22:37:57 -0700

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearchTerm, setSearchTerm] = useState('')
+
+  const regex = new RegExp(newSearchTerm, 'i')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
   }
 
   const handleAddPerson = (event) => {
@@ -15,27 +30,32 @@ const App = () => {
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName, number: newNumber}))
       setNewName('')
     }
   }
-
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search: <input value={newSearchTerm} onChange={handleSearch}/>
+      </div>
+      <h2>Add New Entry</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
-        <div>debug: {newName}</div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
         <div>
           <button type="submit" onClick={handleAddPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => 
-          <li key={person.name}>{person.name}</li>
+        {persons.filter((person) => regex.test(person.name)).map((person) => 
+          <li key={person.name}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
