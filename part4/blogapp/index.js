b630301ce642e76ac8a4b26a8a6976d01dea8425
srtalaie <srt4721@yahoo.com>
2022-05-08 14:57:37 -0700

    4.2 config

--- part4/blogapp/index.js
+++ part4/blogapp/index.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blog')
+const { PORT, MONGODB_URI } = require('./utils/config')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -14,7 +14,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 
-const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
