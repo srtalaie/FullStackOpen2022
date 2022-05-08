c4a3894614ba70c3250a8d5a5a9e1b03c5ae6ea2
srtalaie <srt4721@yahoo.com>
2022-05-08 15:07:58 -0700

    4.2 middleware

--- part4/blogapp/app.js
+++ part4/blogapp/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blog')
 const { MONGODB_URI } = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 logger.info('connecting to', MONGODB_URI)
 
@@ -18,8 +19,13 @@ mongoose.connect(MONGODB_URI)
 
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
