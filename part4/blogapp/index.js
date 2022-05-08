3ca0c3866122097c95567e21e6cd3ea5aa4322d1
srtalaie <srt4721@yahoo.com>
2022-05-08 15:08:37 -0700

    4.2

--- part4/blogapp/index.js
+++ part4/blogapp/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 const http = require('http')
-const { PORT, MONGODB_URI } = require('./utils/config')
+const { PORT } = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
