22991524718ea8fc2dc2055373d6b76387a2915a
srtalaie <srt4721@yahoo.com>
2022-05-08 15:45:53 -0700

    4.4

--- part4/blogapp/utils/list_helper.js
+++ part4/blogapp/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    let initialVal = 0
+    const reducedBlogs = blogs.reduce((prevValue, currentValue) => {
+        return prevValue + currentValue.likes
+    }, initialVal)
+
+    return blogs.length === 0 ? 0 : reducedBlogs
+
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
