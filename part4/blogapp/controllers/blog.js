a37df7a859e9cec78eda6c0743a68efd38c039cd
srtalaie <srt4721@yahoo.com>
2022-05-13 15:17:35 -0700

    added routes for test

--- part4/blogapp/controllers/blog.js
+++ part4/blogapp/controllers/blog.js
@@ -1,4 +1,5 @@
 const blogRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require('../models/blog')
 
 blogRouter.get('/', async (request, response) => {
@@ -19,6 +20,38 @@ blogRouter.post('/', async (request, response) => {
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
 
+})
+
+blogRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.find({ _id: request.params.id })
+    response.json(blog)
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+    const deletedBlog = await Blog.findOneAndDelete({ _id: request.params.id })
+
+      if (deletedBlog) {
+        response.send(deletedBlog)
+        response.status(204).end()
+      } else {
+        response.status(404).end()
+      }
+})
+
+blogRouter.put('/:id', async (request, response) => {
+    const blog = {
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes
+    }
+    const updatedBlog = await Blog.findOneAndUpdate({ _id: request.params.id }, blog,  { new: true, runValidators: true, context: 'query' })
+
+    if (updatedBlog) {
+        response.json(updatedBlog)
+    } else {
+        response.status(404).end()
+    }
 })
 
 module.exports = blogRouter
