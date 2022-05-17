a5d9abe925612512f0ea5369ca8b1fd2b2af67db
srtalaie <srt4721@yahoo.com>
2022-05-17 15:55:02 -0700

    updated creating blog with token auth

--- part4/blogapp/controllers/blogs.js
+++ part4/blogapp/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,8 +18,15 @@ blogRouter.get('/', async (request, response) => {
   
 blogRouter.post('/', async (request, response) => {
     const body = request.body
+    
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
 
-    const user = await User.findById(body.user)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+      }
+      
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
