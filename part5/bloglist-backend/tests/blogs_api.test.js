725caaec4e82f2d6cf377dd8598e6299d9606fbc
srtalaie <srt4721@yahoo.com>
2022-05-25 15:34:35 -0700

    set up for module 5

---
+++ part5/bloglist-backend/tests/blogs_api.test.js
@@ -0,0 +1,151 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  console.log('cleared')
+
+  helper.initialBlogs.forEach(async (blog) => {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+    console.log('saved')
+  })
+  console.log('done')
+})
+
+describe('Server response', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+})
+
+describe('Database indexed correctly', () => {
+    test('unique identifier _id exists', async () => {
+        const response = await api.get('/api/blogs')
+        const blogs = response.body
+        blogs.forEach((blog) => {
+            expect(blog._id).toBeDefined()
+        })
+    })
+})
+
+describe('User able to preform basic functions in DB', () => {
+    let token
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash("pass1234", 10)
+        const user = await new User({ username: "NEW_USER", passwordHash }).save()
+    
+        const userSignIn = { username: "NEW_USER", id: user.id }
+        return (token = jwt.sign(userSignIn, process.env.SECRET))
+    })
+    
+    test('able to create new blog post and blog is saved to DB', async () => {
+        const newBlog = {
+            title: 'New Blog',
+            author: 'New Author',
+            url: 'www.blog.com',
+            likes: 9000
+        }
+    
+        await api
+            .post('/api/blogs')
+            .set("Authorization", `Bearer ${token}`)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+    
+        const response = await api.get('/api/blogs')
+    
+        expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    })
+
+    test('if user is able to successfully delete a blog', async () => {
+        const newBlog = {
+            title: 'New Blog',
+            author: 'New Author',
+            url: 'www.blog.com',
+            likes: 9000
+        }
+
+        await api
+            .post('/api/blogs')
+            .set("Authorization", `Bearer ${token}`)
+            .send(newBlog)
+
+        const response = await api.get('/api/blogs')
+
+        const blogToBeDeletedID = response.body.at(-1)._id
+    
+        await api
+            .delete(`/api/blogs/${blogToBeDeletedID}`)
+            .set("Authorization", `Bearer ${token}`)
+    
+        const newResponseWithoutDeletedBlog = await Blog.find({ _id: blogToBeDeletedID})
+    
+        expect(newResponseWithoutDeletedBlog).toEqual([])
+    })
+    
+    test('if user is able to successfully update a blog', async () => {
+        const response = await api.get('/api/blogs')
+        const blogToBeUpdatedID = response.body[0]._id
+    
+        await api
+            .put(`/api/blogs/${blogToBeUpdatedID}`)
+            .send({likes: 123})
+    
+    
+        const newResponseWithUpdatedBlog = await Blog.find({ _id: blogToBeUpdatedID})
+        const likes = newResponseWithUpdatedBlog[0].likes
+    
+        expect(likes).toEqual(123)
+    })
+
+    test('if likes property is missing, default to 0', async () => {
+        const newBlog = {
+            title: 'New Blog',
+            author: 'New Author',
+            url: 'www.blog.com',
+        }
+    
+        await api
+            .post('/api/blogs')
+            .set("Authorization", `Bearer ${token}`)
+            .send(newBlog)
+            .expect(201)
+    
+        const blogWithZeroLikes = await Blog.find({ title: 'New Blog' })
+    
+        expect(blogWithZeroLikes[0].likes).toEqual(0)
+    })
+    
+    test('if missing title/url then api responds with 400 error', async () => {
+        const newBadBlog = {
+            author: 'New Author',
+        }
+    
+        await api
+            .post('/api/blogs')
+            .set("Authorization", `Bearer ${token}`)
+            .send(newBadBlog)
+            .expect(400)
+    
+    })
+})
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
