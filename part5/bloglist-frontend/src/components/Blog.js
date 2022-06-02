8d654080cef6bc8d217377abf51f5a8a0b13053f
srtalaie <srt4721@yahoo.com>
2022-06-02 15:48:15 -0700

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,30 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleVisibility = () => {
+    setIsVisible(!isVisible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title} - {blog.author} <button onClick={handleVisibility}>{isVisible ? 'hide' : 'view'}</button>
+      {isVisible ? 
+        <div>
+          <div>link: {blog.url}</div>
+          <div>likes: {blog.likes}<button>like</button></div>
+        </div> : <></>
+      }
+    </div>
+  ) 
+}
 
 export default Blog
