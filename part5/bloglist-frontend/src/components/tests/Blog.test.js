66de811fec122e683b9f113f70859f363a453546
srtalaie <srt4721@yahoo.com>
2022-06-06 16:42:53 -0700

    5.14

--- part5/bloglist-frontend/src/components/tests/Blog.test.js
+++ part5/bloglist-frontend/src/components/tests/Blog.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../Blog'
 
-test('Blog component renders blog with title and author, not Likes/Link initially', () => {
+describe('<Blog />', () => {
+  let container
   const blog = {
     title: 'This is a Test Blog',
     author: 'Test Blogger',
@@ -11,13 +13,35 @@ test('Blog component renders blog with title and author, not Likes/Link initiall
     likes: 1
   }
 
-  const { container } = render(<Blog blog={blog} />)
+  beforeEach(() => {
+    container = render(<Blog blog={blog} />).container
+  })
 
-  const visibleElements = screen.getByText(`${blog.title} - ${blog.author}`)
-  const blogLink = container.querySelector('.blog-link')
-  const blogLikes = container.querySelector('.blog-likes')
+  test('Blog component renders blog with title and author, not Likes/Link initially', () => {
+    const visibleElements = screen.getByText(`${blog.title} - ${blog.author}`)
+    const blogLink = container.querySelector('.blog-link')
+    const blogLikes = container.querySelector('.blog-likes')
 
-  expect(visibleElements).toBeDefined()
-  expect(blogLink).toBeNull()
-  expect(blogLikes).toBeNull()
+    expect(visibleElements).toBeDefined()
+    expect(blogLink).toBeNull()
+    expect(blogLikes).toBeNull()
+  })
+
+  test('Clicking on "View/Hide" button shows and hides extra blog content', async () => {
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const blogLink = container.querySelector('.blog-link')
+    const blogLikes = container.querySelector('.blog-likes')
+
+    expect(blogLink).toBeDefined()
+    expect(blogLikes).toBeDefined()
+
+    const hideButton = screen.getByText('hide')
+    await user.click(hideButton)
+
+    expect(blogLink).toBeDefined()
+    expect(blogLikes).toBeDefined()
+  })
 })
