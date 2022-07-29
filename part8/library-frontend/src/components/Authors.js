6392786decaca361a7917a0f59f12062bca1ae43
srtalaie <srt4721@yahoo.com>
2022-07-29 13:21:31 -0700

    8.8

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,30 +1,40 @@
-const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+import { useQuery } from "@apollo/client"
+import { ALL_Authors } from "../queries"
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+const Authors = (props) => {
+	const authors = useQuery(ALL_Authors)
+
+	if (!props.show) {
+		return null
+	}
+
+	return (
+		<div>
+			{authors.loading ? (
+				<div>...loading</div>
+			) : (
+				<div>
+					<h2>authors</h2>
+					<table>
+						<tbody>
+							<tr>
+								<th></th>
+								<th>born</th>
+								<th>books</th>
+							</tr>
+							{authors.data.allAuthors.map((a) => (
+								<tr key={a.name}>
+									<td>{a.name}</td>
+									<td>{a.born}</td>
+									<td>{a.bookCount}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default Authors
