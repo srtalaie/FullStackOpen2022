341c71fdf20d25b0133e21dd13e277af668ae712
srtalaie <srt4721@yahoo.com>
2022-07-29 16:28:30 -0700

    8.12

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { useQuery, useMutation } from "@apollo/client"
+import Select from "react-select"
+
 import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries"
 
 const Authors = (props) => {
@@ -15,7 +17,7 @@ const Authors = (props) => {
 	const handleEditAuthor = (event) => {
 		event.preventDefault()
 
-		editAuthor({ variables: { name, setBornTo: birthYear } })
+		editAuthor({ variables: { name: name.value, setBornTo: birthYear } })
 
 		setName("")
 		setBirthYear("")
@@ -25,48 +27,54 @@ const Authors = (props) => {
 		return null
 	}
 
+	console.log(authors)
+
 	return (
 		<div>
 			{authors.loading ? (
 				<div>...loading</div>
 			) : (
 				<div>
-					<h2>authors</h2>
-					<table>
-						<tbody>
-							<tr>
-								<th></th>
-								<th>born</th>
-								<th>books</th>
-							</tr>
-							{authors.data.allAuthors.map((author) => (
-								<tr key={author.name}>
-									<td>{author.name}</td>
-									<td>{author.born}</td>
-									<td>{author.bookCount}</td>
+					<div>
+						<h2>authors</h2>
+						<table>
+							<tbody>
+								<tr>
+									<th></th>
+									<th>born</th>
+									<th>books</th>
 								</tr>
-							))}
-						</tbody>
-					</table>
+								{authors.data.allAuthors.map((author) => (
+									<tr key={author.name}>
+										<td>{author.name}</td>
+										<td>{author.born}</td>
+										<td>{author.bookCount}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+					<div>
+						<form onSubmit={handleEditAuthor}>
+							<Select
+								value={name}
+								onChange={setName}
+								options={authors.data.allAuthors.map((author) => ({
+									value: author.name,
+									label: author.name,
+								}))}
+							/>
+							<input
+								type="number"
+								label="born"
+								value={birthYear}
+								onChange={({ target }) => setBirthYear(parseInt(target.value))}
+							></input>
+							<button type="submit">edit author</button>
+						</form>
+					</div>
 				</div>
 			)}
-			<div>
-				<form onSubmit={handleEditAuthor}>
-					<input
-						type="text"
-						label="name"
-						value={name}
-						onChange={({ target }) => setName(target.value)}
-					></input>
-					<input
-						type="number"
-						label="born"
-						value={birthYear}
-						onChange={({ target }) => setBirthYear(parseInt(target.value))}
-					></input>
-					<button type="submit">edit author</button>
-				</form>
-			</div>
 		</div>
 	)
 }
