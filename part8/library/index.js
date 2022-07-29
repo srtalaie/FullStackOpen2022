be2100187eca3735bbbeb6e698d80de949e07a7b
srtalaie <srt4721@yahoo.com>
2022-07-28 17:13:29 -0700

    8.7

--- part8/library/index.js
+++ part8/library/index.js
@@ -117,6 +117,7 @@ const typeDefs = gql`
       author: String!
       genres: [String!]
     ) : Book
+    editAuthor(name: String!, setBornTo: Int!) : Author
   }
 `
 
@@ -152,6 +153,12 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((author) => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
