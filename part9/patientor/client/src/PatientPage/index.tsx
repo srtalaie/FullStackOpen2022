07c108bcdf0ca83f5317daab653bba61ed91b43f
srtalaie <srt4721@yahoo.com>
2022-09-16 16:40:03 -0700

    9.17

---
+++ part9/patientor/client/src/PatientPage/index.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import axios from "axios";
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { useParams, useNavigate } from "react-router-dom";
+import { useStateValue } from "../state";
+import { useEffect,useState } from "react";
+
+const PatientPage = () => {
+  const navigate = useNavigate();
+  const [patient, setPatient] = useState<Patient | any>();
+  const [{ cache }, dispatch] = useStateValue();
+  const { id } = useParams<{ id: string }>();
+
+  const getPatient = async () => {
+    try {
+      if (id && id in cache) {
+        setPatient(cache[id]);
+      } else {
+        const {data: fetchedPatient}: Patient | any = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+        setPatient(fetchedPatient);
+        dispatch({ type: "ADD_PATIENT_TO_CACHE", payload: fetchedPatient });
+      }
+    } catch (error) {
+      navigate('/');
+    }
+  };
+  
+  useEffect(() => {
+    void getPatient();
+  }, []);
+
+  if (patient === undefined) {
+    return <div>...loading</div>;
+  } else {
+    return (
+      <div>
+        <h1>{patient.name}</h1>
+        <p>Gender: {patient.gender}</p>
+        <p>Occupation: {patient.occupation}</p>
+        <p>SSN: {patient.ssn}</p>
+      </div>
+    );
+  }
+};
+
+export default PatientPage;
