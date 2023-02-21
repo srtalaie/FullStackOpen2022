07f23267808b8a8aa1bd138a1efbcd2e488524b5
srtalaie <srt4721@yahoo.com>
2023-02-21 15:05:29 -0800

    8.9

--- part8/graphQL-refresher/library-frontend/src/components/Books.js
+++ part8/graphQL-refresher/library-frontend/src/components/Books.js
@@ -1,32 +1,52 @@
+import { gql, useQuery } from "@apollo/client"
+
+const ALL_BOOKS = gql`
+	query {
+		allBooks {
+			title
+			author {
+				name
+			}
+			published
+		}
+	}
+`
+
 const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+	const books = useQuery(ALL_BOOKS)
 
-  const books = []
+	if (!props.show) {
+		return null
+	}
 
-  return (
-    <div>
-      <h2>books</h2>
+	console.log(books)
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+	if (books.loading) {
+		return <div>loading...</div>
+	}
+
+	return (
+		<div>
+			<h2>books</h2>
+
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>author</th>
+						<th>published</th>
+					</tr>
+					{books.data.allBooks.map((a) => (
+						<tr key={a.title}>
+							<td>{a.title}</td>
+							<td>{a.author.name}</td>
+							<td>{a.published}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
 export default Books
